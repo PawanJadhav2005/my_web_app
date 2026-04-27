54b7ec50a2d9e07a661b2ddabc8606f2403fe74e
PawanJadhav2005 <157152902+PawanJadhav2005@users.noreply.github.com>
2026-04-27 10:47:10 +0530

    Fix: added package.json

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-JavaScript
-
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD Web App 🚀');
+  res.send('Hello CI/CD');
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
