206eb6640469a550cb631bd7110b08b8641cd319
PawanJadhav2005 <157152902+PawanJadhav2005@users.noreply.github.com>
2026-04-27 10:06:18 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+JavaScript
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from CI/CD Web App 🚀');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
